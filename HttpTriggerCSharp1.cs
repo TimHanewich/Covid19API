7643507e621d3b0e73915b35d35ed27be3507041
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-06-05 17:38:57 -0400

    Added a V2 version that returns the object, not a string (serialized version of the object)

--- HttpTriggerCSharp1.cs
+++ HttpTriggerCSharp1.cs
@@ -95,6 +95,84 @@ namespace Covid19ApiCore
             return ToReturn;
         }
 
+        [FunctionName("GetDataV2")]
+        public static async Task<Area> GetDataV2([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req, ILogger log)
+        {
+            await Task.Delay(10);
+            string FilterString = req.Query["filter"];
+            string latString = req.Query["latitude"];
+            string lonString = req.Query["longitude"];
+
+            log.LogInformation("Filter: " + FilterString);
+            log.LogInformation("Latitude: " + latString);
+            log.LogInformation("Longitude: " + lonString);
+
+            List<string> FilterParts = new List<string>();
+            if (FilterString != null)
+            {
+                List<string> Splitter = new List<string>();
+                Splitter.Add(",");
+                string[] parts = FilterString.Split(Splitter.ToArray(), StringSplitOptions.None);
+                foreach (string p in parts)
+                {
+                    FilterParts.Add(p.Replace("_"," "));
+                }
+                log.LogInformation(FilterParts.Count.ToString() + " filters");
+            }
+            else
+            {
+                log.LogInformation("No filters supplied!");
+            }
+
+            log.LogInformation("Downloading data...");
+            CovidDataHelper cdh = new CovidDataHelper();
+            Area global = await cdh.GetGlobalDataAsync();
+
+            //Filtering based on the filters
+            Area core = null;
+            if (FilterParts.Count > 0)
+            {
+                log.LogInformation("Filtering data...");
+                core = cdh.ChainFilter(global, FilterParts.ToArray());
+            }
+
+            if (core == null)
+            {
+                throw new Exception();
+            }
+
+
+            //Sort by distance
+            if (latString != null && lonString != null)
+            {
+                
+                log.LogInformation("Converting lat & lon to float...");
+                float mlat = 0;
+                float mlon = 0;
+                try
+                {
+                    mlat = Convert.ToSingle(latString);
+                    mlon = Convert.ToSingle(lonString);
+                }
+                catch
+                {
+                    throw new Exception();
+                }
+                
+                log.LogInformation("Sorting based on provided latitude and longitude...");
+                Area[] sorted = cdh.DistanceSort(core.areas, mlat, mlon);
+
+                //Set it as the areas child now
+                core.areas = sorted;
+            }
+            else
+            {
+                log.LogInformation("No need to sort by distance because a lat and long were not provided.");
+            }
+
+            return core;
+        }
+
 
     }
 }
